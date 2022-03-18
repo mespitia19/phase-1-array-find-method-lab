7b5435c6188d49ba4acbae311157f5b7c3c490f9
MaximoEspitia <max19espitia@gmail.com>
2022-03-17 21:54:41 -0500

    commit tree d9796cfe425ac3c180ee27bcc0286416d9f1b135

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(array) {
+    let football = array.find(record => record.result === "W");
+    //return (football.year);
+    if (football) {
+        return (football.year) 
+    } 
+    else {
+        return (undefined)
+    }
+}
